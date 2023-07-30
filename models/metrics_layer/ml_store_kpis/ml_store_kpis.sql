800fc79334869867ed2aa3f095d0194f64dab688
ay-kireeva <140977503+ay-kireeva@users.noreply.github.com>
2023-07-30 22:11:35 +0200

    Update ml_store_kpis.sql

--- models/metrics_layer/ml_store_kpis/ml_store_kpis.sql
+++ models/metrics_layer/ml_store_kpis/ml_store_kpis.sql
@@ -1,1 +1,17 @@
-
+SELECT
+	happened_at AS report_date,
+  ,country
+  ,city
+  ,store_id
+  ,store_name
+  ,COUNT(DISTINCT t.id) AS transactions
+  ,SUM(amount) AS transacted_amount
+FROM 
+{{source('curated_data_shared', 'store’')}} AS s
+JOIN 
+{{source('curated_data_shared', 'device’')}} AS d
+ON s.id = d.store_id
+JOIN 
+{{source('curated_data_shared', 'transaction’')}} AS t
+ON d.id = t.device_id
+GROUP BY 1, 2, 3, 4, 5
