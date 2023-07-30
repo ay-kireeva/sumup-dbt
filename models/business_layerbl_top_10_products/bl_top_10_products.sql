15268eebeea7cad7233cceb4df68a6ad480d1172
ay-kireeva <140977503+ay-kireeva@users.noreply.github.com>
2023-07-30 21:46:31 +0200

    Create bl_top_10_products.sql

---
+++ models/business_layerbl_top_10_products/bl_top_10_products.sql
@@ -0,0 +1,28 @@
+SELECT
+	TOP(product_sku,10) AS product
+,COUNT(*) AS products_sold
+FROM
+	{{source('curated_data_shared', 'transaction’')}}
+– or similar to the one above (TOP is a legacy function)
+SELECT
+	product_name
+	,product_sku
+	,products_sold
+FROM
+	(SELECT
+		product_name
+		,product_sku
+	,COUNT(id) AS products_sold
+	,ROW_NUMBER() OVER (ORDER BY COUNT(id) DESC) AS product_rank
+FROM {{ source('curated_data_shared', 'transaction’')}}
+WHERE {% if is_incremental() %}
+                happened_at >= date_trunc(date_sub(current_date(), interval 1 week), week) 
+AND
+                happened_at <= date_sub(current_date(), interval 1 day)
+            {% else %}
+            	happened_at >= date_trunc(date_sub(current_date(), interval 13 month), month) AND
+                happened_at <= date_sub(current_date(), interval 1 day)
+            {% endif %}
+GROUP BY 1, 2) AS products
+WHERE product_rank <= 10
+ORDER BY 3 DESC
