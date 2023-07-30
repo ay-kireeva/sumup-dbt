8dcc2501dfdaf4b750e942f64bbf5b913b7054c0
ay-kireeva <140977503+ay-kireeva@users.noreply.github.com>
2023-07-30 21:37:24 +0200

    Create bl_device_type.sql

---
+++ models/business_layer/bl_device_type/bl_device_type.sql
@@ -0,0 +1,25 @@
+SELECT
+	device_type
+	,transactions
+,100*RATIO_TO_REPORT(transactions) OVER () AS transaction_percentage
+FROM
+	(SELECT
+		d.type as device_type
+		,COUNT(t.id) AS transactions
+	FROM {{source('curated_data_shared', 'device’')}} d
+	JOIN {{source('curated_data_shared', 'transaction’')}} t
+		ON d.id = t.device_id
+	GROUP BY 1) AS device_transactions
+– or without legacy functions
+SELECT
+	device_type
+	,transactions
+	,100 * transactions / SUM(transactions) OVER () as transaction_percentage
+FROM
+	(SELECT
+		d.type as device_type
+		,COUNT(t.id) AS transactions
+	FROM {{source('curated_data_shared', 'device’')}} d
+	JOIN {{source('curated_data_shared', 'transaction’')}} t
+		ON d.id = t.device_id
+	GROUP BY 1) AS device_transactions
