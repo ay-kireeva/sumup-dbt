1729fa1400054f0e654a94e1a126e55047602a0b
ay-kireeva <140977503+ay-kireeva@users.noreply.github.com>
2023-07-30 21:45:39 +0200

    Create bl_top_10_stores.sql

---
+++ models/business_layer/bl_top_10_stores/bl_top_10_stores.sql
@@ -0,0 +1,26 @@
+SELECT
+	store_id
+	,store_name
+	,transacted_amount
+FROM
+	(SELECT
+		s.id AS store_id
+	,store_name
+	,SUM(amount) AS transacted_amount
+	,ROW_NUMBER() OVER (ORDER BY SUM(amount) DESC) AS store_rank
+FROM {{source('curated_data_shared', 'store’')}} AS s
+JOIN {{source('curated_data_shared', 'device’')}} AS d
+ON s.id = d.store_id
+JOIN {{source('curated_data_shared', 'transaction’')}} AS t
+ON d.id = t.device_id
+GROUP BY 1, 2) AS store_amount
+WHERE store_rank <= 10
+AND {% if is_incremental() %}
+                happened_at >= date_trunc(date_sub(current_date(), interval 1 week), week) AND
+                happened_at <= date_sub(current_date(), interval 1 day)
+            {% else %}
+              happened_at >= date_trunc(date_sub(current_date(), interval 13 month), month) AND
+                happened_at <= date_sub(current_date(), interval 1 day)
+            {% endif %}
+
+ORDER BY 3 DESC
