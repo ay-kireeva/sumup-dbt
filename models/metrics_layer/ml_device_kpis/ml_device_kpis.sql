14e862f215705ed1305fb7273fe8caaab9779e64
ay-kireeva <140977503+ay-kireeva@users.noreply.github.com>
2023-07-30 22:17:45 +0200

    Update ml_device_kpis.sql

--- models/metrics_layer/ml_device_kpis/ml_device_kpis.sql
+++ models/metrics_layer/ml_device_kpis/ml_device_kpis.sql
@@ -1,1 +1,18 @@
-
+SELECT
+happened_at AS report_date,
+,d.id AS device_id
+,device_type
+,country
+,city
+,COUNT(DISTINCT t.id) AS transactions
+	,SUM(amount) AS transacted_amount
+	,COUNT(DISTINCT card_number) AS customers
+FROM 
+{{source('curated_data_shared', 'device’')}} d
+JOIN 
+{{source('curated_data_shared', 'transaction’')}} t
+ON d.id = t.device_id
+JOIN
+	{{source('curated_data_shared', 'store’')}} AS s
+	ON s.id = d.store_id
+GROUP BY 1, 2, 3, 4, 5
