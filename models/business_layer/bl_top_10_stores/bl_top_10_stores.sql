5a0a0a4c61f9f39403f6b7353e80ccc486e15614
ay-kireeva <140977503+ay-kireeva@users.noreply.github.com>
2023-07-30 22:23:14 +0200

    Update bl_top_10_stores.sql

--- models/business_layer/bl_top_10_stores/bl_top_10_stores.sql
+++ models/business_layer/bl_top_10_stores/bl_top_10_stores.sql
@@ -5,15 +5,15 @@ SELECT
 FROM
 	(SELECT
 		s.id AS store_id
-	,store_name
-	,SUM(amount) AS transacted_amount
-	,ROW_NUMBER() OVER (ORDER BY SUM(amount) DESC) AS store_rank
-FROM {{source('curated_data_shared', 'store’')}} AS s
-JOIN {{source('curated_data_shared', 'device’')}} AS d
-ON s.id = d.store_id
-JOIN {{source('curated_data_shared', 'transaction’')}} AS t
-ON d.id = t.device_id
-GROUP BY 1, 2) AS store_amount
+		,store_name
+		,SUM(amount) AS transacted_amount
+		,ROW_NUMBER() OVER (ORDER BY SUM(amount) DESC) AS store_rank
+	FROM {{source('curated_data_shared', 'store’')}} AS s
+	JOIN {{source('curated_data_shared', 'device’')}} AS d
+	ON s.id = d.store_id
+	JOIN {{source('curated_data_shared', 'transaction’')}} AS t
+	ON d.id = t.device_id
+	GROUP BY 1, 2) AS store_amount
 WHERE store_rank <= 10
 AND {% if is_incremental() %}
                 happened_at >= date_trunc(date_sub(current_date(), interval 1 week), week) AND
