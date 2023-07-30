1390f49d4cd1ca99790d4ad1b33564aff662c792
ay-kireeva <140977503+ay-kireeva@users.noreply.github.com>
2023-07-30 22:20:41 +0200

    Update ml_product_kpis.sql

--- models/metrics_layer/ml_product_kpis/ml_product_kpis.sql
+++ models/metrics_layer/ml_product_kpis/ml_product_kpis.sql
@@ -1,1 +1,17 @@
-
+SELECT
+happened_at AS report_date,
+,product_name
+,product_sku
+,category_name
+,country
+,COUNT(DISTINCT t.id) AS transactions
+	,SUM(amount) AS transacted_amount
+FROM 
+	{{source('curated_data_shared', 'transaction’')}} t
+JOIN 
+	{{source('curated_data_shared', 'device’')}} d
+ON d.id = t.device_id
+JOIN
+	{{source('curated_data_shared', 'store’')}} AS s
+	ON s.id = d.store_id
+GROUP BY 1, 2, 3, 4, 5
